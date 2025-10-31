bdc0c755a27ef57cec2f4dc02a212828202468cc
notdead858-beep <notdead858@gmail.com>
2025-10-31 19:40:27 +0700

    update

---
+++ js/signup.js
@@ -0,0 +1,26 @@
+// validation form register and register user local storage
+const inputUsernameRegister = document.querySelector(".form-control1");
+const inputPasswordRegister = document.querySelector(".form-control2");
+const btnRegister = document.querySelector(".signup__signInButton");
+
+// validation form register and register user local storage
+
+btnRegister.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (
+    inputUsernameRegister.value === "" ||
+    inputPasswordRegister.value === ""
+  ) {
+    alert("vui lòng không để trống ô nào");
+  } else {
+    // array user
+    const user = {
+      username: inputUsernameRegister.value,
+      password: inputPasswordRegister.value,
+    };
+    let json = JSON.stringify(user);
+    localStorage.setItem(inputUsernameRegister.value, json);
+    alert("Đăng Ký Thành Công");
+    window.location.href = "login.html";
+  }
+});
